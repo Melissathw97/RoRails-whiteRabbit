f6a478d1427974815d6381b2a21e7c93f4143d1a
Robinahood1311 <59414122+Robinahood1311@users.noreply.github.com>
2020-05-28 21:53:00 +0800

    Added category ID column to Task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_134935) do
+ActiveRecord::Schema.define(version: 2020_05_28_135132) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_134935) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
